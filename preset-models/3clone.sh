3b96c02475e0517d21e8d4cf222df3a82d6a4a36
Kyo Sakuyo <59904064+KyoSakuyo@users.noreply.github.com>
2022-08-16 14:09:53 +0800

    Update 3clone.sh

--- preset-models/3clone.sh
+++ preset-models/3clone.sh
@@ -8,7 +8,5 @@ mkdir -p package/myapps
 cd package/myapps
 git clone https://github.com/jerrykuku/luci-theme-argon.git
 git clone https://github.com/jerrykuku/luci-app-argon-config.git
-git clone https://github.com/jerrykuku/luci-app-vssr.git
 git clone https://github.com/jerrykuku/lua-maxminddb.git
-git clone https://github.com/Ausaci/luci-app-nat6-helper.git
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
