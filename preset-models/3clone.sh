8661dadc84fe592801dea000e744aa504e31def0
Kyo Sakuyo <59904064+KyoSakuyo@users.noreply.github.com>
2022-08-16 13:16:31 +0800

    Update 3clone.sh

--- preset-models/3clone.sh
+++ preset-models/3clone.sh
@@ -12,6 +12,3 @@ git clone https://github.com/jerrykuku/luci-app-vssr.git
 git clone https://github.com/jerrykuku/lua-maxminddb.git
 git clone https://github.com/Ausaci/luci-app-nat6-helper.git
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
-git clone -b packages --single-branch https://github.com/xiaorouji/openwrt-passwall.git
-git clone -b luci --single-branch https://github.com/xiaorouji/openwrt-passwall.git luci-app-passwall
-git clone https://github.com/xiaorouji/openwrt-passwall2.git
